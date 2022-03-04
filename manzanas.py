2c9809d2d119c87aab78dfd73e311fddeda11449
Daniel Rojas <l20j45_@gmail.com>
2022-03-04 17:12:22 -0600

    agregue el archivo manzana con while

--- manzanas.py
+++ manzanas.py
@@ -2,19 +2,22 @@
  en esta fecha 04/02/2022 '''
 
 print("Programa que te dice cuanto tienes que pagar")
-
 cantidadManzanas=int(input("Ingresa el numero de manzanas: "))
-costodeManzanas=int(input("Ingresa el costo de las manzanas: "))
-totalaPagar=cantidadManzanas*costodeManzanas
+while cantidadManzanas!=0:
+    costodeManzanas=int(input("Ingresa el costo de las manzanas: "))
+    totalaPagar=cantidadManzanas*costodeManzanas
 
-if cantidadManzanas==18:
-    print("desacuento secreto")
-    descuento=totalaPagar*.25
-elif cantidadManzanas>10:
-    print("descuento del 10 %")
-    descuento=totalaPagar*.10
-else:
-    print("no hay descuento master")
-    descuento=0
-print("descuento: ",descuento)
-print("el cliente va a pagar" ,totalaPagar-descuento, "pesos")
+    if cantidadManzanas==18:
+        print("desacuento secreto")
+        descuento=totalaPagar*.25
+    elif cantidadManzanas>10:
+        print("descuento del 10 %")
+        descuento=totalaPagar*.10
+    else:
+        print("no hay descuento master")
+        descuento=0
+    print("descuento: ",descuento)
+    print("el cliente va a pagar" ,totalaPagar-descuento, "pesos")
+    print("-----------------------------------\n\n")
+    cantidadManzanas=int(input("Ingresa el numero de manzanas: "))
+print("saliendo del sistema")
