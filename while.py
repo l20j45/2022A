cb20096680b11b893cf8a06caf877ae1e3d707a3
Daniel Rojas <l20j45_@gmail.com>
2022-03-04 16:45:42 -0600

    ejercicio while

---
+++ while.py
@@ -0,0 +1,5 @@
+multiplicador=int(input("de que numero quieres conocer la tabla: "))
+numero=0
+while numero <= 100:
+    print(f"{multiplicador} * {numero} = {multiplicador*numero}")
+    numero=numero+1
